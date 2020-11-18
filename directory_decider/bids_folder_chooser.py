1eb8ede87e675d4f06dd9703e425e6bdf6fb91c2
Dean Geckt <t-deangeckt@microsoft.com>
2020-11-18 11:09:25 +0200

    browse to choose main bids folder

--- directory_decider/bids_folder_chooser.py
+++ directory_decider/bids_folder_chooser.py
@@ -6,11 +6,26 @@ class BidsFolderChooser(QFrame):
         super().__init__(*args, **kwargs)
         self.layout = QGridLayout()
         self.setLayout(self.layout)
+        self.bids_main_dir = None
+        self.dir_label = None
 
-        # TODO: placeholder widget, delete later
-        placeholder = QLabel('bids folder chooser')
-        self.layout.addWidget(placeholder, 0, 0)
+        # TODO center the title label
+        title = QLabel('Choose main BIDS folder')
+        self.dir_label = QLabel('')
+        self.layout.addWidget(title, 0, 0)
+        self.layout.addWidget(self.dir_label, 1, 0)
 
         # buttons
         self.bttn_next = QPushButton('next')
-        self.layout.addWidget(self.bttn_next, 1, 0)
+        # TODO smaller browse button under the title
+        self.bttn_browse = QPushButton('browse')
+        self.layout.addWidget(self.bttn_browse, 2, 0)
+        self.layout.addWidget(self.bttn_next, 3, 0)
+        self.bttn_browse.clicked.connect(self.open_folder)
+
+    def open_folder(self):
+        kwargs = {'caption': 'Select Directory'}
+        self.bids_main_dir = str(QFileDialog.getExistingDirectory(**kwargs))
+        # TODO validate bids main folder contain key file
+        # TODO add key file parser
+        self.dir_label.setText(self.bids_main_dir)
