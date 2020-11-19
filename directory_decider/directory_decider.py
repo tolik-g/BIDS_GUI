9387a4821c46df4381fe45b6fcc083f949818822
Dean Geckt <t-deangeckt@microsoft.com>
2020-11-19 15:54:33 +0200

    add state and callbacks so data could pass

--- directory_decider/directory_decider.py
+++ directory_decider/directory_decider.py
@@ -1,13 +1,15 @@
 from PyQt5.QtWidgets import *
 
+from Utils.ui_utils import show_warn_message
 from directory_decider.bids_folder_chooser import BidsFolderChooser
 from directory_decider.folder_mapping import FolderMapping
-from directory_decider.subj_mapping import SubjMapping
+from directory_decider.subject_chooser import SubjectChooser
 
 
 class DirectoryDecider(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.data = {'subject_name': '', 'subject_last_name': ''}
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.show_bids_folder_chooser()
@@ -25,15 +27,29 @@ class DirectoryDecider(QWidget):
 
     def show_subj_mapping(self):
         self.clear_layout()
-        widget = SubjMapping()
+        widget = SubjectChooser(self.subject_modified, self.data)
         widget.bttn_next.clicked.connect(self.show_folder_mapping)
         widget.bttn_back.clicked.connect(self.show_bids_folder_chooser)
         self.layout.addWidget(widget)
 
     def show_folder_mapping(self):
+        if not self.subject_validate():
+            return
         self.clear_layout()
         widget = FolderMapping()
         widget.bttn_next.clicked.connect(lambda: print('done'))
         widget.bttn_back.clicked.connect(self.show_subj_mapping)
         self.layout.addWidget(widget)
+
+    def subject_modified(self, value):
+        self.data['subject_name'] = value[0]
+        self.data['subject_last_name'] = value[1]
+
+    def subject_validate(self):
+        if self.data['subject_name'] == '' or self.data['subject_last_name'] == '':
+            show_warn_message("Oops", "Please fill the subject full name")
+            return False
+        # TODO use bids key file utils here to get subject mapping and fill data with it
+        return True
+
 
--- directory_decider/subject_chooser.py
+++ directory_decider/subject_chooser.py
@@ -1,9 +1,9 @@
 from PyQt5.QtWidgets import *
 
 
-class SubjMapping(QFrame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class SubjectChooser(QFrame):
+    def __init__(self, modify_cb, state):
+        super().__init__()
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
@@ -11,11 +11,14 @@ class SubjMapping(QFrame):
         self.layout.addWidget(title, 0, 0)
 
         # inputs
-        self.name_edit = QLineEdit()
+        self.name_edit = QLineEdit(state['subject_name'])
         self.name_edit.setPlaceholderText('First name')
+        self.name_edit.textChanged.connect(lambda: modify_cb(self.get_subject()))
         self.layout.addWidget(self.name_edit, 1, 1)
-        self.last_name_edit = QLineEdit()
+
+        self.last_name_edit = QLineEdit(state['subject_last_name'])
         self.last_name_edit.setPlaceholderText('Last name')
+        self.last_name_edit.textChanged.connect(lambda: modify_cb(self.get_subject()))
         self.layout.addWidget(self.last_name_edit, 2, 1)
 
         # buttons
@@ -23,3 +26,6 @@ class SubjMapping(QFrame):
         self.bttn_back = QPushButton('back')
         self.layout.addWidget(self.bttn_next, 3, 1)
         self.layout.addWidget(self.bttn_back, 3, 0)
+
+    def get_subject(self):
+        return self.name_edit.text(), self.last_name_edit.text()
