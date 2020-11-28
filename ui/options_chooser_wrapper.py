b0921d8f204b94de14f5cf44a46feb3df7396ed4
Point Blank <ump47g@gmail.com>
2020-11-28 16:41:16 +0200

    enlarged file/folder buttons

--- ui/options_chooser_wrapper.py
+++ ui/options_chooser_wrapper.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
-
+from PyQt5.QtGui import QPixmap, QIcon
+import os
 
 class OptionsChooserWrapper(QFrame):
     def __init__(self):
@@ -15,14 +16,10 @@ class OptionsChooserWrapper(QFrame):
         self.setLayout(self.layout_main)
 
         # center layout setup
-        self.layout_center.setRowStretch(0, 1)
-        self.layout_center.setRowStretch(2, 1)
-        self.layout_center.setColumnStretch(0, 1)
-        self.layout_center.setColumnStretch(3, 1)
-
-        # center layout setup
         self.bttn_file = QPushButton('File')
+        self.bttn_file.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.bttn_folder = QPushButton('Folder')
+        self.bttn_folder.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.layout_center.addWidget(self.bttn_file, 1, 1)
         self.layout_center.addWidget(self.bttn_folder, 1, 2)
 
