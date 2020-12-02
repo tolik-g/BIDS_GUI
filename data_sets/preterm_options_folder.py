fdcf9842c959d617bc9f582579328bc40d4dfd02
Dean Geckt <t-deangeckt@microsoft.com>
2020-12-02 20:40:15 +0200

    preterm - add functional pardigm options

--- data_sets/preterm_options_folder.py
+++ data_sets/preterm_options_folder.py
@@ -5,7 +5,8 @@ class PretermOptionsFolder(BidsOptions):
     def __init__(self):
         super().__init__()
         self.options = {'type': ['fmri'],
-                        'fmri': ['anatomy', 'functional', 'dwi']
+                        'fmri': ['anatomy', 'functional', 'dwi'],
+                        'functional': ['empathy', 'resting state', 'synchrony']
                         }
         self.option_type = BidsOptions.Type.FOLDER
         self.mode_type = BidsOptions.Mode.SINGLE
