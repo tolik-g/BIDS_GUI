2f2c3966bb2de937320c98f411744504e1fb41d0
point_blank <ump47g@gmail.com>
2020-12-05 18:43:27 +0200

    refactored move_dcm() name

--- data/bids_file.py
+++ data/bids_file.py
@@ -12,5 +12,5 @@ class BidsFile:
     def get_file_path(self):
         return self.file_path
 
-    def move_dcm(self, path_out, f_name, compression=True):
+    def move_dcm_folder(self, path_out, f_name, compression=True):
         dcm_to_nifti(self.file_path, path_out, f_name, compression)
